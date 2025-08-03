332429d0d5145d06fce3c07eae976a43d8d9cfaa
Sigizmund <mantalutan@gmail.com>
2025-08-03 17:49:32 -0400

    fix_queries9-12

--- query_10.sql
+++ query_10.sql
@@ -1,7 +1,9 @@
 -- query 10
 SELECT
+	s.session_id,
     p.first_name AS coach_first_name,
     p.last_name AS coach_last_name,
+    s.date AS session_date,
     s.time AS session_time,
     l.address AS session_address,
     s.type AS session_type,
@@ -19,4 +21,5 @@ JOIN teams t ON s.session_id = t.session_id
 JOIN personnel p ON t.coach_id = p.employee_id
 JOIN team_players tp ON t.team_id = tp.team_id
 JOIN club_members cm ON tp.cm_id = cm.cm_id
+WHERE s.location_id = 1 AND s.date >= '2023-01-01' AND s.date < '2024-01-01'
 ORDER BY s.date ASC, s.time ASC;
--- query_12.sql
+++ query_12.sql
@@ -1,17 +1,18 @@
 -- Query 12: Report on team formations for all locations
 -- Period: From September 1st, 2023 to October 31st, 2023
-SELECT 
+SELECT
+    l.location_id,
     l.name AS location_name,
-    COUNT(CASE WHEN s.type = 'train' THEN 1 END) AS total_training_sessions,
-    COUNT(CASE WHEN s.type = 'train' THEN tp.cm_id END) AS total_training_players,
-    COUNT(CASE WHEN s.type = 'game' THEN 1 END) AS total_game_sessions,
-    COUNT(CASE WHEN s.type = 'game' THEN tp.cm_id END) AS total_game_players
+    COUNT(DISTINCT CASE WHEN s.type = 'train' THEN s.session_id END) AS total_training_sessions,
+    COUNT(DISTINCT CASE WHEN s.type = 'train' THEN CONCAT(s.session_id, '_', tp.cm_id) END) AS total_training_players,
+    COUNT(DISTINCT CASE WHEN s.type = 'game' THEN s.session_id END) AS total_game_sessions,
+    COUNT(DISTINCT CASE WHEN s.type = 'game' THEN CONCAT(s.session_id, '_', tp.cm_id) END) AS total_game_players
 FROM sessions s
 JOIN locations l ON s.location_id = l.location_id
 JOIN teams t ON s.session_id = t.session_id
 JOIN team_players tp ON t.team_id = tp.team_id
 WHERE s.date BETWEEN '2023-09-01' AND '2023-10-31'
-GROUP BY l.name
+GROUP BY l.location_id, l.name
 HAVING total_game_sessions >= 4
 ORDER BY total_game_sessions DESC;
 
